447b44f5a340fc3d15faa898801e8da122abbd5e
Rob Fitzgerald <rob.fitzgerald@vantiv.com>
2016-06-24 16:57:38 -0600

    index routes in core library

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 'use strict';
 {
-  // sparkify
-  
-  // added feature: wrap scala vector in tuple with y value
-  // i.e. (y [x,x2,..,xn])
-
-  // comments in all files
-
-  // negative tests
-
+  module.exports = require('./lib/jsonToFeatureVector')
 }
